31783d6b49c07847083aab9e16c70bfce470f607
Ulric <Ulric_Ricardo@outlook.com>
2021-09-09 09:35:13 -0400

    initial structure

---
+++ src/Components/DifferenceCard.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../Styles/DifferenceCard.css';
+
+export default class DifferenceCard extends Component {
+    render() {
+        return (
+            <div className="card">
+                Hey
+            </div>
+        )
+    }
+}
