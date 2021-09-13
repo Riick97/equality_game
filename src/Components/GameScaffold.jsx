13e5751a4ef5017b6612fbbc8999ad0d681d2219
Ulric <ulricricardo@gmail.com>
2021-09-13 15:25:51 -0400

    Changes of class to functional component

--- src/Components/GameScaffold.jsx
+++ src/Components/GameScaffold.jsx
@@ -1,170 +1,170 @@
-import React, { Component } from "react";
+import React, {useState } from "react";
 import DifferenceCard from "./DifferenceCard";
 import ProgressBar from "./ProgressBar";
 import "../Styles/GameScaffold.css";
+import { useTransition, animated } from "react-spring";
 
-export default class GameScaffold extends Component {
-  state = {
-    correctIndex: this.getOneOrZero(),
-    groceries: [
-      "apple",
-      "carrot",
-      "mango",
-      "cherry",
-      "kiwifruit",
-      "watermelon",
-    ],
-    currentSelections: ["apple", "carrot", "mango", "cherry"],
-    cards: [
-      { id: 0, show: true, indexes: [0, 1] },
-      { id: 1, show: true, indexes: [2, 3] },
-    ],
-    result: "empty",
-    answerResults: [null, null, null, null, null],
-    correctAnswers: 0,
-    answerCount: 0,
-    gameFinished: false,
+
+export default function GameScaffold(props) {
+  const [correctIndex, setCorrectIndex] = useState(getOneOrZero());
+  const [groceries] = useState([
+    "apple",
+    "carrot",
+    "mango",
+    "cherry",
+    "kiwifruit",
+    "watermelon",
+  ]);
+  const [currentSelections, setCurrentSelections] = useState(getSelections());
+  const [cards, setCards] = useState([
+    { id: 0, show: true, indexes: [0, 1] },
+    { id: 1, show: true, indexes: [2, 3] },
+  ]);
+  const [result, setResult] = useState("empty");
+  const [answerResults, setAnswerResults] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [answerCount, setAnswerCount] = useState(0);
+  const [gameFinished, setGameFinished] = useState(false);
+
+  function getOneOrZero() {
+    return Math.random() >= 0.5 ? 1 : 0;
   };
 
-  constructor() {
-    super();
-    this.state.currentSelections = this.getSelections();
-  }
-
-  getOneOrZero() {
-    return Math.random() >= 0.5 ? 1 : 0;
-  }
-
-  getSelections = () => {
+  function getSelections() {
     let array = [];
     let i = 0;
     while (i < 4) {
-      let random = Math.floor(Math.random() * this.state.groceries.length);
-      if (!array.includes(this.state.groceries[random])) {
-        array.push(this.state.groceries[random]);
+      let random = Math.floor(Math.random() * groceries.length);
+      if (!array.includes(groceries[random])) {
+        array.push(groceries[random]);
         i++;
       }
     }
     return array;
   };
 
-  handleChoice = (id) => {
-    let updatedAnswers = this.state.answerResults;
-    if (this.state.correctIndex === id) {
-      updatedAnswers[this.state.answerCount] = true;
-      let updatedCorrectAnswers = this.state.correctAnswers + 1;
-      this.setState({
-        result: "correct",
-        answerResults: updatedAnswers,
-        correctAnswers: updatedCorrectAnswers,
-      });
+  const handleChoice = (id) => {
+    let updatedAnswers = answerResults;
+    if (correctIndex === id) {
+      updatedAnswers[answerCount] = true;
+      let updatedCorrectAnswers = correctAnswers + 1;
+      setResult('correct');
+      setAnswerResults(updatedAnswers);
+      setCorrectAnswers(updatedCorrectAnswers)
+     
     } else {
-      updatedAnswers[this.state.answerCount] = false;
-      this.setState({ result: "incorrect", answerResults: updatedAnswers });
+      updatedAnswers[answerCount] = false;
+      setResult('incorrect');
+      setAnswerResults(updatedAnswers);
     }
-    let cards = this.state.cards.map((card) => {
+    let updatedCards = cards.map((card) => {
       card.show = card.id === id;
       return card;
     });
 
-    let updatedCount = this.state.answerCount + 1;
-    this.setState({ cards: cards, answerCount: updatedCount });
+    let updatedCount = answerCount + 1;
+    setCards(updatedCards);
+    setAnswerCount(updatedCount);
   };
 
-  handleNext = () => {
-    if (this.state.answerCount >= this.state.answerResults.length) {
-      this.setState({ gameFinished: true });
+  const handleNext = () => {
+    if (answerCount >= answerResults.length) {
+      setGameFinished(true);
     }
-    let cards = this.state.cards.map((card) => {
+    let updatedCards = cards.map((card) => {
       card.show = true;
       return card;
     });
-    this.setState({
-      result: "empty",
-      correctIndex: this.getOneOrZero(),
-      cards: cards,
-      currentSelections: this.getSelections(),
-    });
+    setResult('empty');
+    setCorrectIndex(getOneOrZero());
+    setCards(updatedCards);
+    setCurrentSelections(getSelections());
   };
 
-  handleRestart = () => {
-    this.setState({
-      answerResults: [null, null, null, null, null],
-      correctAnswers: 0,
-      answerCount: 0,
-      gameFinished: false,
-    });
+  const handleRestart = () => {
+    setAnswerResults([null, null, null, null, null]);
+    setCorrectAnswers(0);
+    setAnswerCount(0);
+    setGameFinished(false);
   };
 
-  render() {
-    if (!this.state.gameFinished) {
-      return (
-        <div className="game">
-          <div className="question">
-            {this.state.result !== "empty" ? (
-              this.state.result === "correct" ? (
-                <div className="title_green">
-                  <h2>You've got the right answer!</h2>
-                </div>
-              ) : (
-                <div className="title_red">
-                  <h2>They are not the same</h2>
-                </div>
-              )
+  if (!gameFinished) {
+    return (
+      <div className="game">
+        <div className="question">
+          {result !== "empty" ? (
+            result === "correct" ? (
+              <div className="title_green">
+                <h2>You've got the right answer!</h2>
+              </div>
             ) : (
-              <h2>Which of these items are a matching pair?</h2>
-            )}
-          </div>
-          <div className="container">
-            {this.state.cards.map((card) => {
-              return (
-                <DifferenceCard
-                  correct={this.state.correctIndex === card.id}
-                  groceries={[
-                    this.state.currentSelections[card.indexes[0]],
-                    this.state.currentSelections[card.indexes[1]],
-                  ]}
-                  id={card.id}
-                  key={card.id}
-                  show={card.show}
-                  onChosen={this.handleChoice}
-                  onNext={this.handleNext}
-                  result={this.state.result}
-                />
-              );
-            })}
-          </div>
-          <div className="container_progress">
-            <ProgressBar answerResults={this.state.answerResults} />
-          </div>
+              <div className="title_red">
+                <h2>They are not the same</h2>
+              </div>
+            )
+          ) : (
+            <h2>Which of these items are a matching pair?</h2>
+          )}
         </div>
-      );
-    } else {
-      return (
-        <div className="game">
-          <div className="results_title">
-            {this.state.correctAnswers >= 3 ? (
-              this.state.correctAnswers >= 5 ? <h2>Wow... Excelent!</h2> : <h2>Nice Job!</h2>
+        <div className="container">
+          {cards.map((card) => {
+            return (
+              <DifferenceCard
+                correct={correctIndex === card.id}
+                groceries={[
+                  currentSelections[card.indexes[0]],
+                  currentSelections[card.indexes[1]],
+                ]}
+                id={card.id}
+                key={card.id}
+                show={card.show}
+                onChosen={handleChoice}
+                onNext={handleNext}
+                result={result}
+              />
+            );
+          })}
+        </div>
+        <div className="container_progress">
+          <ProgressBar answerResults={answerResults} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="game">
+        <div className="results_title">
+          {correctAnswers >= 3 ? (
+            correctAnswers >= 5 ? (
+              <h2>Wow... Excelent!</h2>
             ) : (
-              <h2>You'll do better next time</h2>
-            )}
-          </div>
-          <div className="container_results centered">
-            <div className="ratio">
-              <h3>
-                <span className="title_green">{this.state.correctAnswers}</span>
-                /5
-              </h3>
-            </div>
-            <button onClick={this.handleRestart} className="green_button_big">
-              Go Again
-            </button>
-          </div>
-          <div className="container_progress">
-            <ProgressBar answerResults={this.state.answerResults} />
-          </div>
+              <h2>Nice Job!</h2>
+            )
+          ) : (
+            <h2>You'll do better next time</h2>
+          )}
         </div>
-      );
-    }
+        <div className="container_results centered">
+          <div className="ratio">
+            <h3>
+              <span className="title_green">{correctAnswers}</span>
+              /5
+            </h3>
+          </div>
+          <button onClick={handleRestart} className="green_button_big">
+            Go Again
+          </button>
+        </div>
+        <div className="container_progress">
+          <ProgressBar answerResults={answerResults} />
+        </div>
+      </div>
+    );
   }
 }
